9ca197ba336e7bf3cb9ab736a429486d5f076cdf
Onebluesky882 <onebluesky882@outlook.com>
2025-09-12 15:03:35 +0700

    build : better auth

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,25 +1,8 @@
-import { AuthService } from '@mguay/nestjs-better-auth';
 import { NestFactory } from '@nestjs/core';
-import { toNodeHandler } from 'better-auth/node';
-import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
-dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bodyParser: false });
-  // Access Express instance
-  const expressApp = app.getHttpAdapter().getInstance();
-
-  // Access BetterAuth instance from AuthService
-  const authService = app.get<AuthService>(AuthService);
-
-  expressApp.all(
-    /^\/api\/auth\/.*/,
-    toNodeHandler(authService.instance.handler),
-  );
-
-  expressApp.use(require('express').json());
-  app.setGlobalPrefix('api');
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
