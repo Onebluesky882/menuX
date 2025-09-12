e9b48a14d1bf70e1154fbf727e7210783eab359b
Onebluesky882 <onebluesky882@outlook.com>
2025-09-12 23:05:33 +0700

    fix  : api

--- client/src/Api/shop.api.ts
+++ client/src/Api/shop.api.ts
@@ -1,12 +1,12 @@
 import type { ReceiveBank } from "@/type/qrcode.type";
 import axiosInstance from ".";
 export const shopAPI = {
-  create: (data: any) => axiosInstance.post("/shops", data),
-  getAll: () => axiosInstance.get("/shops"),
-  getById: (id: string) => axiosInstance.get(`/shops/${id}`),
-  update: (id: string, data: any) => axiosInstance.put(`/shops/${id}`, data),
-  delete: (id: string) => axiosInstance.delete(`/shops/${id}`),
-  getOwnerShop: () => axiosInstance.get("/shops/getOwnerShop"),
+  create: (data: any) => axiosInstance.post("api/shops", data),
+  getAll: () => axiosInstance.get("api/shops"),
+  getById: (id: string) => axiosInstance.get(`api/shops/${id}`),
+  update: (id: string, data: any) => axiosInstance.put(`api/shops/${id}`, data),
+  delete: (id: string) => axiosInstance.delete(`api/shops/${id}`),
+  getOwnerShop: () => axiosInstance.get("api/shops/getOwnerShop"),
   patchShopReceiveBank: (shopId: string, data: ReceiveBank) =>
-    axiosInstance.patch(`/shops/${shopId}`, data),
+    axiosInstance.patch(`api/shops/${shopId}`, data),
 };
