4794abb5c85bc351246995824e917eddcbe1469a
Onebluesky882 <onebluesky882@outlook.com>
2025-10-01 22:24:36 +0700

    build : get shop menu

--- consumer/src/api/menu.api.ts
+++ consumer/src/api/menu.api.ts
@@ -2,7 +2,6 @@ import { axiosInstance } from ".";
 
 export const menuApi = {
   getMenuByShopId: (shopId: string) =>
-    axiosInstance.get(`/menus`, { params: { shopId } }),
-  getMenuWithOption: (shopId: string) =>
-    axiosInstance.get(`menus/options/${shopId}`),
+    axiosInstance.get(`api/menus/${shopId}`, { params: { shopId } }),
+  getAllMenu: (shopId: string) => axiosInstance.get(`api/menus/${shopId}`),
 };
