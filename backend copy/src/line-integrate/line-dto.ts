5a3636f48e3a3946bfcd1e96dd42fa5bc27950f3
Onebluesky882 <onebluesky882@outlook.com>
2025-10-03 00:30:32 +0700

    deployment

--- backend copy/src/line-integrate/line-dto.ts
+++
@@ -1,6 +0,0 @@
-export type CreateUserLineDto = {
-  id: string;
-  name: string;
-  email: string;
-  image: string;
-};
--- backend copy/src/order-table/order-table.dto.ts
+++
@@ -1,6 +0,0 @@
-import { InferInsertModel } from 'drizzle-orm';
-import { orderTable } from '../database/schema';
-
-export type InsertOrdersTable = InferInsertModel<typeof orderTable>;
-
-export type CreateOrderTableDto = InsertOrdersTable;
