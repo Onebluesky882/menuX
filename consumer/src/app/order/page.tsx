33ed742c160d6e7133586c19b3a365bb72c00367
Onebluesky882 <onebluesky882@outlook.com>
2025-10-03 21:20:00 +0700

    remove unused

--- consumer/src/app/order/page.tsx
+++
@@ -1,4 +0,0 @@
-const page = () => {
-  return <div>order</div>;
-};
-export default page;
---
+++ consumer/src/components/DotLottieReact/index.tsx
@@ -0,0 +1,21 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+export const QrcodeLiveScan = () => {
+  return <DotLottieReact src="/lottiefiles/GzxFhOpzz0.lottie" loop autoplay />;
+};
+
+export const TouchClick = () => {
+  return (
+    <div className="absolute top-1  pointer-events-none">
+      <DotLottieReact
+        src="/lottiefiles/bXV1RnSf8W.lottie"
+        loop
+        autoplay
+        color="white"
+      />
+    </div>
+  );
+};
+
+export const Cooking = () => {
+  return <DotLottieReact src="/lottiefiles/ewmp8Lg4A7.lottie" loop autoplay />;
+};
